13c9b9df329004973a2c8f0e6ae4d2ba9f09bef4
Sunbabaara <csunbabaara@gmail.com>
2021-03-18 16:44:39 +0000

    second

---
+++ src/components/Heron.js
@@ -0,0 +1,10 @@
+
+ function Heron () {
+     return (
+         <div id="hero-img">
+         <h2>2020 Graduation</h2>
+         <img src="./vero/IMG_3504.jpg" style={{height: "100%", width:"100%"}}/>
+         </div>
+     )
+ }
+ export default Heron;
